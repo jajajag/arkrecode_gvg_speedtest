1d0faff95ef0dbe653f0023cc9dbffe42c53a485
Jag Zhang <jajajag@users.noreply.github.com>
2026-01-08 23:06:52 -0500

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -144,7 +144,7 @@ async def speed_summary(bot, ev: CQEvent):
         lines = []
         for (enemy, enemy_min, enemy_max, mean, med, mode_int, ally_min) in ret:
             enemy_name, note = enemy[0], enemy[3]
-            lines.append(f"{enemy_name}{enemy_min:d}-{note}")
+            lines.append(f"{enemy_name}{mode_int:d}-{note}")
         prefix = f"{title}：" if title else ""
         msg = prefix + "，".join(lines)
         await bot.send(ev, msg, at_sender=False)
